b49ca06d5a97dcb05e0702d069c183b54c131323
MelTripot <tripot.melanie@ynov.com>
2021-03-25 14:58:23 +0100

    Correction du model Projet

--- models/projet.js
+++ models/projet.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const schema   = mongoose.Schema
 
 // https://mongoosejs.com/docs/schematypes.html
-const ContactSchema=new schema({
+const ProjetSchema=new schema({
     nom : { type:String, lowercase: true, required: 'Veuillez donnez un nom a votre projet'},    
     categories:[String],
     porteur:[String],
