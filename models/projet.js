75452728b52dbbb895024d5cd5fc696556b5ee9c
MelTripot <tripot.melanie@ynov.com>
2021-03-25 14:55:49 +0100

    Ajout du schéma et du Model de Projet

---
+++ models/projet.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+const schema   = mongoose.Schema
+
+// https://mongoosejs.com/docs/schematypes.html
+const ContactSchema=new schema({
+    nom : { type:String, lowercase: true, required: 'Veuillez donnez un nom a votre projet'},    
+    categories:[String],
+    porteur:[String],
+    palier:{ type: Number, default: false },
+})
