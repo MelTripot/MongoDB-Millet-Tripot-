856cab82edcf9de41ed1d109d10fb0ca2a06fa5b
MelTripot <tripot.melanie@ynov.com>
2021-03-25 15:00:42 +0100

    Model projet modifié
    suppresion de la notion de porteur

--- models/projet.js
+++ models/projet.js
@@ -5,6 +5,5 @@ const schema   = mongoose.Schema
 const ProjetSchema=new schema({
     nom : { type:String, lowercase: true, required: 'Veuillez donnez un nom a votre projet'},    
     categories:[String],
-    porteur:[String],
     palier:{ type: Number, default: false },
 })
